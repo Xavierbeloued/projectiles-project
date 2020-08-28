14e9421d9a0f0c1687dbd3dc1e2dab0863bc6581
Xavierbeloued <70276929+Xavierbeloued@users.noreply.github.com>
2020-08-28 09:40:16 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,64 +1,95 @@
+# Shoot Enemeies
+
+def on_player1_button_a_pressed():
+    global projectile
+    projectile = sprites.create_projectile_from_sprite(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . 5 5 4 2 . . . . . . 
+                    . . . . . . 5 5 4 2 . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        Player,
+        60,
+        0)
+controller.player1.on_button_event(ControllerButton.A,
+    ControllerButtonEvent.PRESSED,
+    on_player1_button_a_pressed)
+
+projectile: Sprite = None
+Player: Sprite = None
 # Create Game
 info.set_score(0)
 info.set_life(3)
 scene.set_tile_map_level(tilemap("""level"""))
 # Create Player
 Player = sprites.create(img("""
-    . . . . . . f f . . . . . . . .
-    . . . . . . e e . . . . . . . .
-    . . . . . e e e e . . . . . . .
-    . e e e e e e e e e e e e e f f
-    . e e . e e e e 8 e e e e e f .
-    . e e . e e e e e e . . . . . .
-    . e e . e e e e e e . . . . . .
-    . . . . e e e e e e . . . . . .
-    . . . . e e e e e e . . . . . .
-    . . . e e . . . . e e . . . . .
-    . . . e e . . . . e e . . . . .
-    . . . e e . . . . e e . . . . .
-    . . . e e . . . . e e . . . . .
-    . . . f f . . . . f f . . . . .
-    . . . f f f . . . f f f . . . .
-    . . . . . . . . . . . . . . . .
-"""))
+        . . . . . . f f . . . . . . . . 
+            . . . . . . e e . . . . . . . . 
+            . . . . . e e e e . . . . . . . 
+            . e e e e e e e e e e e e e f f 
+            . e e . e e e e 8 e e e e e f . 
+            . e e . e e e e e e . . . . . . 
+            . e e . e e e e e e . . . . . . 
+            . . . . e e e e e e . . . . . . 
+            . . . . e e e e e e . . . . . . 
+            . . . e e . . . . e e . . . . . 
+            . . . e e . . . . e e . . . . . 
+            . . . e e . . . . e e . . . . . 
+            . . . e e . . . . e e . . . . . 
+            . . . f f . . . . f f . . . . . 
+            . . . f f f . . . f f f . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    0)
 scene.camera_follow_sprite(Player)
 controller.move_sprite(Player)
+Player.set_kind(SpriteKind.player)
 # Create Enemies
-Enemy = sprites.create(img("""
-    . . . . . . f f . . . . . . . .
-    . . . . . . f f . . . . . . . .
-    . . . . . . 2 2 . . . . . . . .
-    . . 2 2 2 2 2 2 2 2 2 2 . . . .
-    . . 2 . . 2 2 2 2 . . 2 . . . .
-    . . 2 . . 2 2 2 2 . . 2 . . . .
-    . . . . . 2 2 2 2 . . . . . . .
-    . . . . 2 2 2 2 2 2 . . . . . .
-    . . . . 2 2 2 2 2 2 . . . . . .
-    . . . 2 2 . . . . 2 2 . . . . .
-    . . . 2 2 . . . . 2 2 . . . . .
-    . . . 2 2 . . . . 2 2 . . . . .
-    . . . 2 2 . . . . 2 2 . . . . .
-    . . . f f . . . . f f . . . . .
-    . . . f f f . . . f f f . . . .
-    . . . . . . . . . . . . . . . .
-"""))
-# Shoot Enemeies
-controller.player1.on_button_event(Controller)
-projectile = sprites.create_projectile_from_sprite(img("""
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . 5 5 4 2 . . . . . .
-    . . . . . . 5 5 4 2 . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-"""), Player, 50, 50)
+def on_update_interval():
+    prisoner = sprites.create(img("""
+        . . . . . . f f . . . . . . . . 
+            . . . . . . f f . . . . . . . . 
+            . . . . . . 2 2 . . . . . . . . 
+            . . 2 2 2 2 2 2 2 2 2 2 . . . . 
+            . . 2 . . 2 2 2 2 . . 2 . . . . 
+            . . 2 . . 2 2 2 2 . . 2 . . . . 
+            . . . . . 2 2 2 2 . . . . . . . 
+            . . . . 2 2 2 2 2 2 . . . . . . 
+            . . . . 2 2 2 2 2 2 . . . . . . 
+            . . . 2 2 . . . . 2 2 . . . . . 
+            . . . 2 2 . . . . 2 2 . . . . . 
+            . . . 2 2 . . . . 2 2 . . . . . 
+            . . . 2 2 . . . . 2 2 . . . . . 
+            . . . f f . . . . f f . . . . . 
+            . . . f f f . . . f f f . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    5)
+    prisoner.set_position(scene.screen_width(),randint(0, 200))
+    prisoner.set_velocity(50, 50)
+    prisoner.set_flag(SpriteFlag.BOUNCE_ON_WALL, True)
+    prisoner.set_kind(SpriteKind.enemy)
+game.on_update_interval(1500, on_update_interval)
+# Lose Lives
+def on_overlap(sprite, otherSprite):
+    otherSprite.destroy()
+    info.change_life_by(-1)
+sprites.on_overlap(SpriteKind.player,SpriteKind.enemy, on_overlap)
+
+def on_overlap2(sprite, otherSprite):
+    sprite.destroy()
+    otherSprite.destroy()
+    info.change_score_by(1)
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, on_overlap2)
--- main.ts
+++ main.ts
@@ -1,64 +1,85 @@
+//  Shoot Enemeies
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function on_player1_button_a_pressed() {
+    
+    projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . 5 5 4 2 . . . . . . 
+                    . . . . . . 5 5 4 2 . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        `, Player, 60, 0)
+})
+let projectile : Sprite = null
+let Player : Sprite = null
 //  Create Game
 info.setScore(0)
 info.setLife(3)
 scene.setTileMapLevel(tilemap`level`)
 //  Create Player
-let Player = sprites.create(img`
-    . . . . . . f f . . . . . . . .
-    . . . . . . e e . . . . . . . .
-    . . . . . e e e e . . . . . . .
-    . e e e e e e e e e e e e e f f
-    . e e . e e e e 8 e e e e e f .
-    . e e . e e e e e e . . . . . .
-    . e e . e e e e e e . . . . . .
-    . . . . e e e e e e . . . . . .
-    . . . . e e e e e e . . . . . .
-    . . . e e . . . . e e . . . . .
-    . . . e e . . . . e e . . . . .
-    . . . e e . . . . e e . . . . .
-    . . . e e . . . . e e . . . . .
-    . . . f f . . . . f f . . . . .
-    . . . f f f . . . f f f . . . .
-    . . . . . . . . . . . . . . . .
-`)
+Player = sprites.create(img`
+        . . . . . . f f . . . . . . . . 
+            . . . . . . e e . . . . . . . . 
+            . . . . . e e e e . . . . . . . 
+            . e e e e e e e e e e e e e f f 
+            . e e . e e e e 8 e e e e e f . 
+            . e e . e e e e e e . . . . . . 
+            . e e . e e e e e e . . . . . . 
+            . . . . e e e e e e . . . . . . 
+            . . . . e e e e e e . . . . . . 
+            . . . e e . . . . e e . . . . . 
+            . . . e e . . . . e e . . . . . 
+            . . . e e . . . . e e . . . . . 
+            . . . e e . . . . e e . . . . . 
+            . . . f f . . . . f f . . . . . 
+            . . . f f f . . . f f f . . . . 
+            . . . . . . . . . . . . . . . .
+    `, 0)
 scene.cameraFollowSprite(Player)
 controller.moveSprite(Player)
+Player.setKind(SpriteKind.Player)
 //  Create Enemies
-let Enemy = sprites.create(img`
-    . . . . . . f f . . . . . . . .
-    . . . . . . f f . . . . . . . .
-    . . . . . . 2 2 . . . . . . . .
-    . . 2 2 2 2 2 2 2 2 2 2 . . . .
-    . . 2 . . 2 2 2 2 . . 2 . . . .
-    . . 2 . . 2 2 2 2 . . 2 . . . .
-    . . . . . 2 2 2 2 . . . . . . .
-    . . . . 2 2 2 2 2 2 . . . . . .
-    . . . . 2 2 2 2 2 2 . . . . . .
-    . . . 2 2 . . . . 2 2 . . . . .
-    . . . 2 2 . . . . 2 2 . . . . .
-    . . . 2 2 . . . . 2 2 . . . . .
-    . . . 2 2 . . . . 2 2 . . . . .
-    . . . f f . . . . f f . . . . .
-    . . . f f f . . . f f f . . . .
-    . . . . . . . . . . . . . . . .
-`)
-//  Shoot Enemeies
-controller.player1.onButtonEvent
-let projectile = sprites.createProjectileFromSprite(img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . 5 5 4 2 . . . . . .
-    . . . . . . 5 5 4 2 . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-`, Player, 50, 50)
+game.onUpdateInterval(1500, function on_update_interval() {
+    let prisoner = sprites.create(img`
+        . . . . . . f f . . . . . . . . 
+            . . . . . . f f . . . . . . . . 
+            . . . . . . 2 2 . . . . . . . . 
+            . . 2 2 2 2 2 2 2 2 2 2 . . . . 
+            . . 2 . . 2 2 2 2 . . 2 . . . . 
+            . . 2 . . 2 2 2 2 . . 2 . . . . 
+            . . . . . 2 2 2 2 . . . . . . . 
+            . . . . 2 2 2 2 2 2 . . . . . . 
+            . . . . 2 2 2 2 2 2 . . . . . . 
+            . . . 2 2 . . . . 2 2 . . . . . 
+            . . . 2 2 . . . . 2 2 . . . . . 
+            . . . 2 2 . . . . 2 2 . . . . . 
+            . . . 2 2 . . . . 2 2 . . . . . 
+            . . . f f . . . . f f . . . . . 
+            . . . f f f . . . f f f . . . . 
+            . . . . . . . . . . . . . . . .
+    `, 5)
+    prisoner.setPosition(scene.screenWidth(), randint(0, 200))
+    prisoner.setVelocity(50, 50)
+    prisoner.setFlag(SpriteFlag.BounceOnWall, true)
+    prisoner.setKind(SpriteKind.Enemy)
+})
+//  Lose Lives
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function on_overlap2(sprite: Sprite, otherSprite: Sprite) {
+    sprite.destroy()
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+})
